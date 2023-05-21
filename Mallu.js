0f5d921ecc6fd5bab30a081d194550f994dbadda
mallikarjun2308 <134145221+mallikarjun2308@users.noreply.github.com>
2023-05-21 15:39:08 +0530

    Add files via upload

---
+++ Mallu.js
@@ -0,0 +1,642 @@
+
+//console.log(9+7+6);
+//let m = 'ronaldo'
+//let age = 30;
+//age = 31;
+
+//const birthyear = 1991;
+//birthyear = 1991;
+
+
+//var job = 'programmer';
+//job = 'teacher';
+
+//const now = 2037;
+//const ageharry = now - 1991;
+//const agehenry = now - 1995;
+//console.log(ageharry, agehenry);
+
+//console.log(ageharry * 2, agehenry / 10)
+
+/*const firstname = 'tony';
+const lastname = 'stark';
+console.log(firstname + '' + lastname);*/
+
+//let x = 10 + 5;
+//console.log(x);
+
+/*const now = '90';
+const ageharry = now - 40;
+const agehenry = now - 80;
+console.log(ageharry * 2, agehenry * 2);
+
+console.log(ageharry < agehenry);
+console.log(ageharry - agehenry);
+
+console.log(agehenry >= 18)
+
+let hp = '7';
+const agepeter = hp +  2;
+const agemartin = hp + 4;
+const agetony = hp + 6;
+
+console.log(agepeter + agemartin + agetony);
+console.log(agepeter < agetony);
+
+//let x = 10 + 5;
+//console.log(x += 15);
+
+
+let x = 10;
+console.log(x * 2);
+
+let age = 30;
+var b = 31;
+var a = 32;
+
+console.log(b > a);*/
+
+
+/*const weightJohn = 78;
+const heightjhon = 1.69;
+const weightMark = 92;
+const heightMark = 1.95;
+
+JhonBMI = 78 / 1.69 ** 2;
+MarkBMI = 92 / 1.95 ** 2;
+const MarkhigherBMI = JhonBMI > MarkBMI;
+console.log(JhonBMI, MarkBMI);*/
+
+/*const weightpajya = 40;
+const heightpajya = 1.6; 
+
+pajyaBMI = 40 / 1.6 ** 2;*/
+
+
+
+//tring and templet.
+/*const firstname = 'Mallu';
+const job = 'programmer';
+const birthyear = 2008;
+const year = 2023;
+
+
+const John = `I'm  ${firstname}, a ${year - birthyear} year old ${job}`;
+
+console.log(John);*/
+
+
+/*const age = 19;
+
+
+if(age >= 18){
+    console.log('john can drive the car')
+} else{
+    const leftyear = 18 - age;
+    console.log(`john you are too young, ait for another ${leftyear} year`); 
+}*/
+
+
+
+/*const age = 20;
+
+if(age >= 20){
+    console.log('you can enjoy the ride');
+} else{
+    const minimumAge = 20 - age;
+    console.log(`oops you are not alloed for the ride, ait for another  ${minimumAge}  year;` );
+}
+mappuGubbi*/
+
+//thi i econd challenge
+
+/*const weightJohn = 78;
+const heightjhon = 1.69;
+const weightMark = 92;
+const heightMark = 1.95;
+
+JhonBMI = 78 / 1.69 ** 2;
+MarkBMI = 92 / 1.95 ** 2;
+const MarkhigherBMI = JhonBMI > MarkBMI;
+console.log(JhonBMI, MarkBMI);
+
+if(JhonBMI > MarkBMI) {
+    console.log('Jhon have higher BMI')
+} else{
+    console.log('Mark have higher BMI')
+};*/
+ 
+//const inputyear = '1991';
+//console.log(Number(inputyear), inputyear);
+
+//let n = '1' + 1;
+//n = n - 1;
+//console.log(n);
+
+/*let n = '17' - 1
+n = n - 6;
+console.log(n);*/
+
+// console.log(Boolean(0))
+
+
+
+// if (favourite === 2) {
+//     console.log('too low')
+// } else if (favourite === 3) {
+//     console.log('you  guessed the correct number')
+     
+// } else if (favourite === 4) {
+//     console.log('high')
+// } else if(favourite === 5){
+//     console.log('high')
+// }  else if(favourite === 6) {
+//     console.log('high')
+// } else if(favourite === 7) {
+//     console.log(' high')
+// }  else if (favourite === 8) {
+//     console.log('too high')
+// }  else if (favourite === 9) {
+//     console.log('too high')
+// }   else if (favourite === 10) {
+//     console.log('too too high')
+// }  else {
+//     console.log('hi')
+// }
+
+/*const scoreDolphins = (96 + 89 + 108) / 3;
+const scorekoalas = (88 + 91 + 110) / 3;
+console.log(scoreDolphins, scorekoalas);
+
+if (scoreDolphins > scorekoalas) {
+    console.log('Dolphins won the trophy')
+} else if (scorekoalas > scoreDolphins) {
+    console.log('Koalas won the trophy')
+}  else if(scoreDolphins === scorekoalas) {
+    console.log('Both team won the match')
+};*/
+
+// const favourite = Number(prompt("guess the number between 1 to 10"))
+
+// const secretNumber= Math.trunc(Math.random()*10)+1;
+// if(secretNumber===favourite){
+//     console.log(`${secretNumber}, is the answer, your guess is correct}`)
+// } else if(favourite<secretNumber){
+//     console.log('too low')
+//     Number(prompt("guess the number between 1 to 10"))
+// }  else if(favourite>secretNumber) {
+//     console.log('too high')
+//     Number(prompt("guess the number between 1 to 10"))
+// 
+
+//const luckynumber = 7;
+
+//if(luckynumber===3) 
+//let color = [orange, blue, red];
+
+//color[0] = 'red'
+
+//const comment = [
+ //   {peronName: 'herry', mode: 'happy', Text: 'loloolololo'}
+//]
+//con
+
+// onst animals = ["tiger", "lion"]
+// for (let i = 0; i < animals.length; i++){
+//     console.log(i, animals[i])
+// }
+// const aanimal = ["tiger", "lion"]
+// for (let i = 0; i < animal.length; i++){
+//     console.log(i, animal[i])
+// }
+
+
+// const eatingfood = ['kalakhan', 'gobi65', 'curry', 'momo', 'naan']
+// for (let i = 0; i < eatingfood.length; i++){
+//    const coloumn = eatingfood[i];
+//    {console.log(`coloumn  ${i +1}`)}
+//    for (let j = 0; j < coloumn.length; j++ ){
+//     console.log(coloumn[j])
+//    }
+
+// const secretNumber = "77";
+
+// let guess = prompt(" guess the secretNumber and check in console wheather your number is correct or not")
+//     while (guess !== secretNumber){
+//     guess = prompt(" guess the secretNumber and check in console wheather your number is correct or not")
+    
+//         }
+// console.log("correct")
+
+//  let maimum = parseInt(prompt("Guess A maimum Number"));
+//  while(!maimum){
+//     maimum = parseInt(prompt("Guess A Valid Number"));
+//  }
+//   const number = Math.floor(Math.random() * maimum) + 1;
+//  console.log(maimum)
+//   let Guess = parseInt(prompt("Guess The random number"));
+  
+//   while(Guess !== number)  ;
+//   if(Guess > number){
+//      Guess = parseInt(prompt('Too high'));
+//    } else{
+//      Guess = parseInt(prompt('Too low'));
+//    }
+
+// let mom = parseInt(prompt('Random number'));
+// while(!mom){
+//     mom = parseInt(prompt('not a valid number'))
+// }
+//     number = Math.floor(Math.random() * mom) + 1;
+//     console.log(mom);
+//     let Guess = parseInt(prompt('find the hidden number'))
+//     while(Guess !== number)
+//     if(Guess > number){
+//              Guess =  console.log (prompt('too high')) ;
+//     } else (Guess < number);{
+//             Guess = console.log(prompt('too lo'))
+//     }
+  
+//   console.log(`You got it,You took ${attempt} Guess`);
+ // onmoueenter
+// 
+// const button = document.querySelector('button');
+// button.addEventListener('click', function() {
+   
+//       const r = Math.floor(Math.random() * 255);
+//       const g = Math.floor(Math.random() * 255);
+//       const b = Math.floor(Math.random() * 255);
+//      const color = `rgb(${r}, ${g}, ${b})`;
+//    document.body.style.backgroundColor = color;
+//  })
+ 
+//  const boom = document.querySelector('h1');
+//  for(let h1 of h1s){
+//     h1.addEventListener('click', function() {
+//         h1.style.backgroundColor = rgb;
+//     })
+//  }
+//  const Makerandcolor = () => {
+//     const r = Math.floor(Math.random() * 255);
+//       const g = Math.floor(Math.random() * 255);
+//       const b = Math.floor(Math.random() * 255);
+//      const color = `rgb(${r}, ${g}, ${b})`;
+//  }
+//  const buttons = document.querySelectorAll('buttons');
+
+//  for (let button of buttons){
+//     button.addEventListener('click', function() {
+//         button.style.backgroundColor = Makerandcolor;
+//     })
+//  }
+ // const button = document.querySelector('button');
+//  button.addEventListener('click', function(){
+ //       const r = Math.floor(Math.random() * 225);
+//     const g = Math.floor(Math.random() * 225);
+//     const b = Math.floor(Math.random() * 225);
+//     const color = `rgb(${r}, ${g}, ${b})`;
+//     document.body.style.backgroundColor = color;
+//  })
+
+//  const buttons = document.querySelector('button')
+ 
+//  for(let button of buttons){
+//     button.addeventlistener('click', function(){
+//         console.lof('clicked')
+//     })
+//  }
+// const Makerandcolor = () => {
+// const r = Math.floor(Math.random() * 255);
+//      const g = Math.floor(Math.random() * 255);
+//        const b = Math.floor(Math.random() * 255);
+//       return `rgb(${r}, ${g}, ${b})`;
+// }
+
+// const buttons = document.querySelectorAll(button);
+
+// for (let button of buttons){
+//     button.addeventlistener('Click', function() {
+//      console.log('clicked')
+//     })
+// }
+
+
+// 
+
+// const googleForm = document.querySelector('#googleForm')
+// googleForm.addEventListener('submit', function(e) {
+//     console.log('done')
+//     e.preventDefault();
+
+//     const username = googleForm.element.username.value;
+//     const form =  googleForm.element.form.value;
+//     const newForm = document.createElement('li');
+//      const btag = document.createElement('b');
+//      btag.append(username)
+//      newForm.append(btag);
+//     newForm.append(`${google}`)
+//      console.log(newForm)
+// })
+
+
+
+
+// const tweetForm = document.querySelector('#tweetForm');
+// const tweetsContainer = document.querySelector('#tweets');
+// tweetForm.addEventListener('submit', function (e) {
+//     e.preventDefault();
+
+//     // const usernameInput = document.querySelectorAll('input')[0];
+//     // const tweetInput = document.querySelectorAll('input')[1];
+//     const usernameInput = tweetForm.elements.username;
+//     const tweetInput = tweetForm.elements.tweet;
+//     addTweet(usernameInput.value, tweetInput.value)
+//     usernameInput.value = '';
+//     tweetInput.value = '';
+// });
+
+// const addTweet = (username, tweet) => {
+//     const newTweet = document.createElement('li');
+//     const bTag = document.createElement('b');
+//     bTag.append(username)
+//     newTweet.append(bTag);
+//     newTweet.append(`- ${tweet}`)
+//     tweetsContainer.append(newTweet);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Guess game!
+
+//  let maimum = parseInt(prompt("Guess A maimum Number"));
+//  while(!maimum){
+//     maimum = parseInt(prompt("Guess A Valid Number"));
+//  }
+//   const number = Math.floor(Math.random() * maimum) + 1;
+//  console.log(maimum)
+//   let Guess = parseInt(prompt("Guess The random number"));
+  
+//   while(Guess !== number)  ;
+//   if(Guess > number){
+//      Guess = parseInt(prompt('Too high'));
+//    } else{
+//      Guess = parseInt(prompt('Too low'));
+//    }
+
+
+// Click bg color change
+
+// const button1 = document.querySelector('#button1');
+// button1.addEventListener('click', function() {
+   
+//       const r = Math.floor(Math.random() * 255);
+//       const g = Math.floor(Math.random() * 255);
+//       const b = Math.floor(Math.random() * 255);
+//      const color = `rgb(${r}, ${g}, ${b})`;
+//    document.body.style.backgroundColor = color;
+//  })
+
+
+// form //
+
+// const tweetForm = document.querySelector('#tweetForm');
+// const tweetsContainer = document.querySelector('#tweets');
+// tweetForm.addEventListener('submit', function (e) {
+//     e.preventDefault();
+
+//     // const usernameInput = document.querySelectorAll('input')[0];
+//     // const tweetInput = document.querySelectorAll('input')[1];
+//     const usernameInput = tweetForm.elements.username;
+//     const tweetInput = tweetForm.elements.tweet;
+//     addTweet(usernameInput.value, tweetInput.value)
+//     usernameInput.value = '';
+//     tweetInput.value = '';
+// });
+
+// const addTweet = (username, tweet) => {
+//     const newTweet = document.createElement('li');
+//     const bTag = document.createElement('b');
+//     bTag.append(username)
+//     newTweet.append(bTag);
+//     newTweet.append(`- ${tweet}`)
+//     tweetsContainer.append(newTweet);
+// }
+
+
+
+
+
+
+
+
+
+// alert(hello);
+
+// const p1Button = document.querySelector('#p1Button');
+// const p2Button = document.querySelector('#p2Button');
+//  const p1display = document.querySelector('#p1display');
+//  const p2display = document.querySelector('#p2display');
+
+
+//  let p1Score = 0;
+
+// p1Button.addEventListener('click', function() {
+//     alert(hello);
+//      p1Button += 1;
+//      p1display.textContent = p1Score;
+// })
+
+
+
+// setTimeout(() => {
+//     document.body.style.backgroundColor = 'red';
+// }, 1000);
+// setTimeout(() => {
+//     document.body.style.backgroundColor = 'blue';
+// }, 2000);
+
+// 
+
+// const fetchBitcoinPrice = async () => {
+//     try {
+//         const res = await axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
+//         console.log(res.data.ticker.price)
+//     } catch (e) {
+//         console.log("ERROR!", e)
+//     }
+// }
+
+// const getDadJoke = async() =>{
+//     const config = { headers: { Accept: 'application/json' } }
+//     const res = await axios.get('https://icanhazdadjoke.com/')
+//     console.log(res.data.joke)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fetchBitcoinPrice = async () => {
+//     try {
+//         const res = await axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
+//         console.log(res.data.ticker.price)
+//     } catch (e) {
+//         console.log("ERROR!", e)
+//     }
+// }
+
+
+
+// const jokes = document.querySelector('#jokes');
+//  const button = document.querySelector('button');
+
+//  const addNewJoke = async () => {
+//      const jokeText = await getDadJoke();
+//      const newLI = document.createElement('Li');
+//      newLI.append(jokeText);
+//      jokes.append(newLI)
+//  }
+
+//  const getDadJoke = async () => {
+//      try{
+//          const config = {headers: { Accept: 'application/json' } }
+//          const res = await axios.get('https://icanhazdadjoke.com/', config)
+//          return res.data.joke;
+//      } catch (e) {
+//          return "no joke, goo"
+//      }
+     
+     
+// }
+//    button.addEventListener('click', addNewJoke)
+
+
+
+
+const joke = document.querySelector('#jokes');
+const button = document.querySelector('#button2');
+
+const addNewJoke = async () => {
+    const jokeText = await getDadJoke();
+    const newLI = document.createElement('li');
+    newLI.append(jokeText);
+    joke.append(newLI);
+}
+
+const getDadJoke = async () => {
+    try{
+        const config = {headers: { Accept: 'application/json'} }
+        const res = await axios.get('https://icanhazdadjoke.com/', config)
+        return res.data.joke;
+    } catch (e) {
+        return ""
+    }
+}
+ button.addEventListener('click', addNewJoke);
+
+
+
+  const button1 = document.querySelector('#button1');
+   button1.addEventListener('click', function() {
+    
+         const r = Math.floor(Math.random() * 255);
+         const g = Math.floor(Math.random() * 255);
+         const b = Math.floor(Math.random() * 255);
+        const color = `rgb(${r}, ${g}, ${b})`;
+      document.body.style.backgroundColor = color;
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
